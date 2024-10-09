6901963bf8f6c743b99377248e9456fd31dcba17
Dhrubajyoti Rakshit <dhrubajyoti.rakshit@vistex.com>
2024-10-10 00:52:05 +0200

    Added common-types as a package

---
+++ common-types/index.ts
@@ -0,0 +1,12 @@
+// Example types
+export type User = {
+    id: string;
+    name: string;
+    email: string;
+};
+
+export type Resume = {
+    userId: string;
+    content: string;
+    createdAt: Date;
+};
